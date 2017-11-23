9053d3b3115b9659387c2741377f9cd3b1c17880
Xavier Le Tohic <xavier.letohic@gmail.com>
2017-11-23 16:45:59 +0100

    Added antd

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,12 @@
+import Layout from '../index.js'
+import { LocaleProvider } from 'antd'
+import frFR from 'antd/lib/locale-provider/fr_FR'
+
+
 export default () => (
-    <div>
-        Hello !
-    </div>
+    <Layout>
+        <LocaleProvider locale={frFR}>
+            <h1>Hello world !</h1>
+        </LocaleProvider>
+    </Layout>
 )
---
+++ theme.js
@@ -0,0 +1,6 @@
+module.exports = () => {
+    return {
+        'primary-color': '#5C6AD3',
+        'link-color': '#108ee9'
+    };
+};
